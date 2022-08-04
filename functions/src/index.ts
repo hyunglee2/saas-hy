21604ea48619573484e1afd6aacd50c1c8956d86
hyunglee2 <63332045+hyunglee2@users.noreply.github.com>
2022-08-04 12:08:29 +0900

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,4 +7,4 @@ export { onUserCreate, onUserDelete } from './users';
 
 // Uncomment if you deploy to a different provider than Netlify and want to use NetlifyCMS in production
 // Make sure you follow the steps provided in the readme
-// export { oauthAuthorize, oauthCallback } from './oauth';
+export { oauthAuthorize, oauthCallback } from './oauth';
